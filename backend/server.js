412a02592372f7e4071c0f45da7ac7f67fb48649
Mangesh Rajguru <msrajguru7@gmail.com>
2026-02-22 14:17:10 +0530

    fix(server): 0.0.0.0 bind

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running in ${process.env.NODE_env || 'production'} mode on port ${PORT}`);
 });
